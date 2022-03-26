762079a2d7be6469cc3b04898fd511b2a183d8b6
testersadik <62357426+testersadik@users.noreply.github.com>
2022-03-26 13:56:54 +0100

    Create github-actions-script.sh

---
+++ github-actions-script.sh
@@ -0,0 +1,16 @@
+          echo What is in directory
+          ls -a
+          echo 
+          echo is java installed
+          java -version
+          echo
+          echo is git installed
+          git --version
+          echo
+          echo what about build tools
+          mvn --version
+          gradle --version
+          ant -version
+          echo
+          echo What environment variable  are avaible?
+          env
